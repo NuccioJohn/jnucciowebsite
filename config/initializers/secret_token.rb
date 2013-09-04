da40faa5083c65f336da87e868fd554032246be8
John Nuccio <nuccio.john@gmail.com>
2013-09-04 14:19:11 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jnucciowebsite::Application.config.secret_token = '7ad3f82314dffa8ce6714bf95638553eee306d64c943a9dc1e16d0b598112b93879974196213d69b4af48a8cd80807b1313b716ae89e26540f5d49cdb2828c5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jnucciowebsite::Application.config.session_store :cookie_store, key: '_jnucciowebsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jnucciowebsite::Application.config.session_store :active_record_store
