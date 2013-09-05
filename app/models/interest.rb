0ad7b94b67a04e28673e048ba9c31b7a344d25bc
John Nuccio <nuccio.john@gmail.com>
2013-09-05 10:36:46 -0700

    fixed a mistake with interest

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,6 @@
 class Interest < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :title
 
   validates :description, presence: true
+  validates :title, presence: true
 end
